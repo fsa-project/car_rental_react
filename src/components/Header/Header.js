b04af4eab5ea9e0f78175c9234d5b12b0ad24568
minhdangfptu <minhdangdinh261004@gmail.com>
2024-11-22 09:26:33 +0700

    update sang 22

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,52 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Logo from '../../assets/logo51.png'
-import { NavLink, useNavigate } from 'react-router-dom';
-import { Offcanvas } from 'react-bootstrap';
-import Avatar from '../../assets/static/image/avatar-default.jpg'
-import './Header.scss'
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Logo from "../../assets/logo51.png";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Offcanvas } from "react-bootstrap";
+import Avatar from "../../assets/static/image/avatar-default.jpg";
+import "./Header.scss";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-  //const account = useSelector((state) => state.user.account); // Trỏ đến state.user
-  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const isAuthenticated = useSelector((state) => state.user?.isAuthenticated);
 
   const [showDropdown, setShowDropdown] = useState(false);
-  const handleToggle = (isOpen) => {
-    setShowDropdown(isOpen);
-  };
-
-  const handleImageClick = () => {
-    setShowDropdown(!showDropdown);
-  }
 
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    navigate('/auth');
-  }
+    navigate("/auth");
+  };
+  const handleProfile = () => {
+    navigate("/edit-profile");
+  };
+  const handleBooking = () => {
+    navigate("/my-booking");
+  };
+  const handleWallet = () => {
+    navigate("/wallet");
+  };
   const handleRegister = () => {
-    navigate('/auth');
-  }
+    navigate("/auth");
+  };
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+    window.location.reload();
+  };
+
+  const handleToggleDropdown = () => {
+    setShowDropdown((prev) => !prev);
+  };
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid="sm">
-        <NavLink className='navbar-brand' to={"/"}>
+        <NavLink className="navbar-brand" to={"/"}>
           <img
             src={Logo}
             width="100%"
@@ -47,7 +56,8 @@ const Header = () => {
           />
         </NavLink>
         <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
-        <Navbar.Offcanvas id="offcanvasNavbar-expand-lg"
+        <Navbar.Offcanvas
+          id="offcanvasNavbar-expand-lg"
           aria-labelledby="offcanvasNavbarLabel-expand-lg"
           placement="end"
         >
@@ -57,59 +67,66 @@ const Header = () => {
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav
-              className="justify-content-end flex-grow-1 pe-3 text-align-center"
-            >
-              <NavLink to={"/"} className='nav-link header-link'>My Ride</NavLink>
-              <NavLink to={"/"} className='nav-link header-link'>Become to Renter</NavLink>
-              <NavLink to={"/"} className='nav-link header-link'>About us</NavLink>
-              <div class="vr"></div>
+            <Nav className="justify-content-end flex-grow-1 pe-3 text-align-center">
+              <NavLink to={"/"} className="nav-link header-link">
+                My Ride
+              </NavLink>
+              <NavLink to={"/"} className="nav-link header-link">
+                Become to Renter
+              </NavLink>
+              <NavLink to={"/"} className="nav-link header-link">
+                About us
+              </NavLink>
+              <div className="vr"></div>
             </Nav>
             <Nav>
-              {
-                isAuthenticated === true ?
-                  <>
-                    <div className="btn-auth">
-                      <Button variant='light btn-login' onClick={() => handleLogin()}>Sigh in</Button>
-                      <Button variant='outline-dark btn-register' onClick={() => handleRegister()}>Sign up</Button>
-                    </div>
-                  </>
-                  :
-                  <>
-                    <div className="dropdown-profile">
-                      <div className="avatar avatar--s" onClick={handleImageClick}>
-                        <img src={Avatar} loading='lazy' />
-                      </div>
-                      <NavDropdown
-                        title="Do Thuat"
-                        id="offcanvasNavbarDropdown-expand-lg"
-                        show={showDropdown}
-                        onClick={() => handleToggle(true)}
-                        onMouseLeave={() => handleToggle(false)}
-                      >
-                        <NavDropdown.Item >My profile</NavDropdown.Item>
-                        <NavDropdown.Item >My Bookings</NavDropdown.Item>
-                        <NavDropdown.Item >My Wallet</NavDropdown.Item>
-                        <NavDropdown.Item >Log out</NavDropdown.Item>
-                      </NavDropdown>
-                    </div>
-                  </>
-              }
-
-
-
-
-
+              {!isAuthenticated ? (
+                <div className="btn-auth">
+                  <Button variant="light btn-login" onClick={handleLogin}>
+                    Sign in
+                  </Button>
+                  <Button
+                    variant="outline-dark btn-register"
+                    onClick={handleRegister}
+                  >
+                    Sign up
+                  </Button>
+                </div>
+              ) : (
+                <div className="dropdown-profile">
+                  <div
+                    className="avatar avatar--s"
+                    onClick={handleToggleDropdown}
+                  >
+                    <img src={Avatar} loading="lazy" alt="User Avatar" />
+                  </div>
+                  <NavDropdown
+                    title="Do Thuat"
+                    id="offcanvasNavbarDropdown-expand-lg"
+                    show={showDropdown}
+                    onClick={handleToggleDropdown}
+                  >
+                    <NavDropdown.Item onClick={handleProfile}>
+                      My profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={handleBooking}>
+                      My Bookings
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={handleWallet}>
+                      My Wallet
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={handleLogout}>
+                      Log out
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </div>
+              )}
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
       </Container>
     </Navbar>
-
-
-
-
-  )
-}
+  );
+};
 
 export default Header;
